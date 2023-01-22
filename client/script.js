c70c3e7c94aeff4942839981dd1093dfe2092700
jainishgamit1025 <jainishgamit374@gmail.com>
2023-01-22 12:55:09 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -88,7 +88,7 @@ const handleSubmit = async (e) => {
 
     //featch data from server -> bot's response
 
-    const response = await fetch('http://localhost:5000',{
+    const response = await fetch('https://codeai-kok1.onrender.com',{
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
